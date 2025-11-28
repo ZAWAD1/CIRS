7503204db6df97cc2f96d9ea2c0d601ac49afa75
ZAWAD1 <zawad.b.a.k@gmail.com>
2025-11-28 17:30:17 +0600

    Created student dashboard, Emergency Contact

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import RoleSelect from "./pages/RoleSelect";
 import StudentAllReport from "./pages/StudentAllReport";
+import StudentDashboard from "./pages/StudentDashboard";
+import StudentEmergencyContact from "./pages/StudentEmergencycontact";
+import EmergencyContacts from "./pages/EmergencyContacts";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/roles" element={<RoleSelect />} />
       <Route path="/student/reports" element={<StudentAllReport />} />
+      <Route path="/student/dashboard" element={<StudentDashboard />} />
+      <Route path="/emergency" element={<StudentEmergencyContact />} />
+      <Route path="/home-emergency" element={<EmergencyContacts />} />
     </Routes>
   );
 }
