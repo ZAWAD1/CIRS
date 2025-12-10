7cebc3f0e42ec36187b93ba5ba6bbe735ec71973
Zawad Bin Abu Kayum <91911652+ZAWAD1@users.noreply.github.com>
2025-12-10 19:16:48 +0600

    Update StudentAllReport.tsx

--- frontend/src/pages/StudentAllReport.tsx
+++ frontend/src/pages/StudentAllReport.tsx
@@ -16,7 +16,7 @@ const StudentAllReport = () => {
     const fetchReports = async () => {
       try {
         setLoading(true);
-        const res = await fetch("https://cirs-59j6.onrender.com");
+        const res = await fetch("https://cirs-59j6.onrender.com/reports");
         const data = await res.json();
         setReports(data || []);
       } catch (error) {
